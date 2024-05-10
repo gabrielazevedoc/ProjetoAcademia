65ad62e96bc1199d9de208099c3c42b5386946be
Gabriel Azevedo <gabrielazevedoc15@hotmail.com>
2024-05-10 19:00:08 -0300

    feat: add Pessoa Class

--- ProjetoAcademia/obj/Release/net8.0-windows/ProjetoAcademia.AssemblyInfo.cs
+++ ProjetoAcademia/obj/Release/net8.0-windows/ProjetoAcademia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAcademia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac8cc827903bb4b0fb275608e32c5e96bd9740f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAcademia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAcademia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
