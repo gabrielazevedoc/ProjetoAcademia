0232ad39e700636a67aac6cc6ca844a57fd4f53c
Gabriel Azevedo <gabrielazevedoc15@hotmail.com>
2024-05-20 19:40:44 -0300

    feat: add Professor form

---
+++ ProjetoAcademia/View/Professor_screen.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace ProjetoAcademia.View
+{
+    partial class Professor_screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            textBox8 = new TextBox();
+            label9 = new Label();
+            comboBox1 = new ComboBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Window;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1029, 90);
+            panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Window;
+            label1.FlatStyle = FlatStyle.Popup;
+            label1.Font = new Font("Arial", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.WindowFrame;
+            label1.Location = new Point(372, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(294, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Área do Professor";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(52, 456);
+            button2.Name = "button2";
+            button2.Size = new Size(139, 37);
+            button2.TabIndex = 8;
+            button2.Text = "Cadastrar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.FlatStyle = FlatStyle.Popup;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(52, 132);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 19);
+            label2.TabIndex = 9;
+            label2.Text = "Nome";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(52, 154);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(161, 26);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(52, 216);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(161, 26);
+            textBox2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.FlatStyle = FlatStyle.Popup;
+            label3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(52, 194);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 19);
+            label3.TabIndex = 11;
+            label3.Text = "Idade";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(52, 276);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(161, 26);
+            textBox3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.FlatStyle = FlatStyle.Popup;
+            label4.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(52, 254);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 19);
+            label4.TabIndex = 13;
+            label4.Text = "CPF";
+            label4.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(52, 343);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(161, 26);
+            textBox4.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.FlatStyle = FlatStyle.Popup;
+            label5.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(52, 321);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 19);
+            label5.TabIndex = 15;
+            label5.Text = "Senha";
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(248, 154);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(161, 26);
+            textBox5.TabIndex = 18;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.FlatStyle = FlatStyle.Popup;
+            label6.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(248, 132);
+            label6.Name = "label6";
+            label6.Size = new Size(48, 19);
+            label6.TabIndex = 17;
+            label6.Text = "Peso";
+            label6.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(248, 216);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(161, 26);
+            textBox6.TabIndex = 20;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.FlatStyle = FlatStyle.Popup;
+            label7.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ControlLightLight;
+            label7.Location = new Point(248, 194);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 19);
+            label7.TabIndex = 19;
+            label7.Text = "Altura";
+            label7.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.FlatStyle = FlatStyle.Popup;
+            label8.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = SystemColors.ControlLightLight;
+            label8.Location = new Point(248, 254);
+            label8.Name = "label8";
+            label8.Size = new Size(48, 19);
+            label8.TabIndex = 21;
+            label8.Text = "Sexo";
+            label8.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(248, 343);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(161, 26);
+            textBox8.TabIndex = 24;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.FlatStyle = FlatStyle.Popup;
+            label9.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = SystemColors.ControlLightLight;
+            label9.Location = new Point(248, 321);
+            label9.Name = "label9";
+            label9.Size = new Size(57, 19);
+            label9.TabIndex = 23;
+            label9.Text = "Treino";
+            label9.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Masculino" });
+            comboBox1.Location = new Point(248, 276);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(161, 26);
+            comboBox1.TabIndex = 25;
+            // 
+            // Professor_screen
+            // 
+            AutoScaleDimensions = new SizeF(9F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(994, 540);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox8);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(panel1);
+            Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Professor_screen";
+            Text = "Professor_screen";
+            Load += Professor_screen_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Button button2;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox8;
+        private Label label9;
+        private ComboBox comboBox1;
+    }
+}
